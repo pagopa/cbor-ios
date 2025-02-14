5796727dc27ee0ba17348a02d8f12388e4c54466
hevelius <mario.perrotta@pagopa.it>
2025-02-14 15:25:39 +0100

    chore: use github env workspace

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cd cbor
 xcodebuild archive \
     -scheme cbor \
     -destination "generic/platform=iOS Simulator" \
-    -archivePath "../archives/cbor-iOS-simulator.xcarchive" \
+    -archivePath "${GITHUB_WORKSPACE}/archives/cbor-iOS-simulator.xcarchive" \
     -configuration Release \
     -sdk iphonesimulator \
     ONLY_ACTIVE_ARCH=NO \
@@ -18,15 +18,16 @@ xcodebuild archive \
 # iOS Devices
 xcodebuild archive \
     -scheme cbor \
-    -archivePath "../archives/cbor-iOS.xcarchive" \
+    -archivePath "${{ github.workspace }}/archives/cbor-iOS.xcarchive" \
     -destination "generic/platform=iOS" \
     -configuration Release \
     -sdk iphoneos \
     ONLY_ACTIVE_ARCH=NO \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
 # Build cbor.xcframework
 xcodebuild -create-xcframework \
-    -framework "../archives/cbor-iOS.xcarchive/Products/Library/Frameworks/cbor.framework" \
-    -framework "../archives/cbor-iOS-simulator.xcarchive/Products/Library/Frameworks/cbor.framework" \
-    -output "../archives/cbor.xcframework"
+    -framework "${{ github.workspace }}/archives/cbor-iOS.xcarchive/Products/Library/Frameworks/cbor.framework" \
+    -framework "${{ github.workspace }}/archives/cbor-iOS-simulator.xcarchive/Products/Library/Frameworks/cbor.framework" \
+    -output "${{ github.workspace }}/archives/cbor.xcframework"
