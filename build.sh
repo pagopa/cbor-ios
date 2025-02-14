04cee9875b544c495db6f4b07fe93411db0aaf00
hevelius <mario.perrotta@pagopa.it>
2025-02-14 15:42:32 +0100

    revert: workspace path

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cd cbor
 xcodebuild archive \
     -scheme cbor \
     -destination "generic/platform=iOS Simulator" \
-    -archivePath "${GITHUB_WORKSPACE}/archives/cbor-iOS-simulator.xcarchive" \
+    -archivePath "../archives/cbor-iOS-simulator.xcarchive" \
     -configuration Release \
     -sdk iphonesimulator \
     ONLY_ACTIVE_ARCH=NO \
@@ -18,16 +18,16 @@ xcodebuild archive \
 # iOS Devices
 xcodebuild archive \
     -scheme cbor \
-    -archivePath "${GITHUB_WORKSPACE}/archives/cbor-iOS.xcarchive" \
+    -archivePath "../archives/cbor-iOS.xcarchive" \
     -destination "generic/platform=iOS" \
     -configuration Release \
     -sdk iphoneos \
     ONLY_ACTIVE_ARCH=NO \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
-
+    
 # Build cbor.xcframework
 xcodebuild -create-xcframework \
-    -framework "${GITHUB_WORKSPACE}/archives/cbor-iOS.xcarchive/Products/Library/Frameworks/cbor.framework" \
-    -framework "${GITHUB_WORKSPACE}/archives/cbor-iOS-simulator.xcarchive/Products/Library/Frameworks/cbor.framework" \
-    -output "${GITHUB_WORKSPACE}/archives/cbor.xcframework"
+    -framework "../archives/cbor-iOS.xcarchive/Products/Library/Frameworks/cbor.framework" \
+    -framework "../archives/cbor-iOS-simulator.xcarchive/Products/Library/Frameworks/cbor.framework" \
+    -output "../archives/cbor.xcframework"
